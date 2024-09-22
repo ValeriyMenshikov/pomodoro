a66933a67cd637c5f3b204e017d5b7f53d44c737
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2024-09-22 20:38:47 +0300

    использую другой сервис отправки сообщений

--- worker/celery.py
+++ worker/celery.py
@@ -9,17 +9,25 @@ from app.settings import Settings
 
 settings = Settings()
 
-celery = Celery(
-    __name__,
-    broker=settings.CELERY_RABBIT_URL,
-    backend="rpc://",
-)
+celery = Celery(__name__)
 
+celery.conf.broker_url = settings.CELERY_REDIS_URL
+celery.conf.result_backend = settings.CELERY_REDIS_URL
+
+
+# app.conf.update(
+#     result_backend='redis://localhost:6379/0',
+#     task_serializer='json',
+#     accept_content=['json'],
+#     result_serializer='json',
+#     timezone='Europe/London',
+#     enable_utc=True,
+# )
 
 @celery.task(name='send_email_task')
 def send_email_task(subject: str, text: str, to: str):
     msg = _build_message(subject, text, to)
-    _send_email_mailhog(msg)
+    _send_email(msg)
 
 
 def _build_message(subject: str, text: str, to: str) -> MIMEMultipart:
@@ -32,6 +40,9 @@ def _build_message(subject: str, text: str, to: str) -> MIMEMultipart:
     return msg
 
 
-def _send_email_mailhog(msg: MIMEMultipart) -> None:
-    with smtplib.SMTP(settings.SMTP_HOST, settings.SMTP_PORT) as server:
-        server.send_message(msg)
+def _send_email(msg: MIMEMultipart) -> None:
+    context = ssl.create_default_context()
+    server = smtplib.SMTP_SSL(settings.SMTP_HOST, settings.SMTP_PORT, context=context)
+    server.login(settings.MAIL_FROM, settings.SMTP_PASSWORD)
+    server.send_message(msg)
+    server.quit()
