247e935b50300144ba70e1a29af1d513a535b953
Menshikov Valeriy Sergeevich <vmenshikov@ozon.ru>
2024-10-02 00:29:28 +0300

    docker победил

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,9 @@ FROM python:3.12-slim
 WORKDIR /app
 
 COPY pyproject.toml poetry.lock /app/
+COPY app /app/app
 
 RUN pip install poetry
-RUN poetry lock --no-update
-RUN poetry install
-
-
-COPY . /app
-
+RUN poetry install --no-interaction --no-ansi
 
 CMD ["poetry", "run", "uvicorn", "app.main:app", "--host", "0.0.0.0", "--port", "8081", "--reload"]
--- app/settings.py
+++ app/settings.py
@@ -2,13 +2,13 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DB_HOST: str = "0.0.0.0"
+    DB_HOST: str = "postgres"
     DB_PORT: int = 5432
     DB_USER: str = "postgres"
     DB_PASSWORD: str = "password"
     DB_DRIVER: str = "postgresql+asyncpg"
     DB_NAME: str = "pomodoro"
-    CACHE_HOST: str = "0.0.0.0"
+    CACHE_HOST: str = "redis"
     CACHE_PORT: int = 6379
     CACHE_DB: int = 0
     JWT_SECRET: str = "secret"
@@ -21,8 +21,8 @@ class Settings(BaseSettings):
     YANDEX_CLIENT_SECRET: str = ""
     YANDEX_REDIRECT_URI: str = ""
     YANDEX_TOKEN_URL: str = "https://oauth.yandex.ru/token"
-    CELERY_REDIS_URL: str = "redis://localhost:6379/1"
-    RABBIT_URL: str = "amqp://guest:guest@localhost:5672//"
+    CELERY_REDIS_URL: str = "redis://redis:6379/1"
+    RABBIT_URL: str = "amqp://guest:guest@rabbit:5672//"
 
     @property
     def db_url(self):
